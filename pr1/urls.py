9477c3a8f5b8b9657b54d00f2827461f1a6c2c0e
Vishal Thakur <Vishalthakur40786@gmail.com>
2026-07-04 21:22:00 +0530

    Update urls.py

--- pr1/urls.py
+++ pr1/urls.py
@@ -15,18 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('cafe.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("cafe.urls")),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
 
-admin.site.site_header = 'Home of Flavours Admin'
-admin.site.index_title = 'Home of Flavours Admin Portal'
-admin.site.site_title = 'Home of Flavours'
+admin.site.site_header = "Home of Flavours Admin"
+admin.site.index_title = "Home of Flavours Admin Portal"
+admin.site.site_title = "Home of Flavours"
